d0b9d5f6387a2149413ab3d5ce2e66663a244b7a
Pasan Anuththara <pasananuththara19@gmail.com>
2023-07-23 23:27:41 +0530

    Fixed Dashboard connection issue

--- connection.php
+++ connection.php
@@ -1,5 +1,11 @@
 <?php
-    $connection = new mysqli("localhost", "id21049122_galleryofgalleriesadmin", "Gallery#of@Galleries%2023", "id21049122_adminlogin");
+
+    $session = "localhost";
+    $username = "id21049122_galleryofgalleriesadmin";
+    $password = "Gallery#of@Galleries%2023";
+    $database_name = "id21049122_adminlogin";
+    
+    $connection = new mysqli($session, $username, $password, $database_name);
 
     if ($mysqli -> connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
