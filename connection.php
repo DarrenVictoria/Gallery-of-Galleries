21b9f472490f79ccff9b9f08c736068dfb87518e
Isira Adithya <rp.isira@gmail.com>
2023-07-23 20:25:12 +0530

    Test code

--- connection.php
+++ connection.php
@@ -2,6 +2,14 @@
     // $connection = mysqli_connect("localhost", "root", "", "adminlogin");
     $connection = new mysqli("localhost", "root", "", "adminlogin");
 
+    $sql = "SELECT Username FROM usercredentials";   
+
+    $users = $conn->query($sql);
+
+    $users->fetch_all();
+
+    print_r($exhibitions);
+
     if ($mysqli -> connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
         exit();
